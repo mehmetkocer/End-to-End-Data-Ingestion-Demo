8b950f7a77d7f3aeaa4293448cdbc76d580f4762
Mehmet Kocer <mehmetkocer@protonmail.com>
2021-08-16 22:01:45 +0300

    Modules folder added.

---
+++ modules/lambda.tf
@@ -0,0 +1,11 @@
+data "aws_iam_role" "role1" {
+  name = "data-intern-glue-job"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = data.aws_iam_role.role1.arn
+  handler       = "index.test"
+
+}
---
+++ modules/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "eu-west-1"
+    shared_credentials_file = "/home/mehmet/.aws/credentials"
+}
+
