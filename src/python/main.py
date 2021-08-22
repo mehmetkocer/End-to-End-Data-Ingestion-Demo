5f34aa57b9ecda5444f8c88a4343240c839da9c6
Mehmet Kocer <mehmetkocer@protonmail.com>
2021-08-22 21:42:28 +0300

    main.py modified

--- src/python/main.py
+++ src/python/main.py
@@ -1,16 +1,20 @@
 import json
 import boto3
-import requests
 
-s3_path = '/'
-bucket = 'intern-mehmetkocer'
+# trigger the glue job
+
+glueJobName = "intern-mehmet-kocer-converter-job"
+
+def lambda_handler(event, context):
+    client_glue = boto3.client('glue')
+
+    client_glue.start_job_run(
+        JobName = glueJobName,
+        arguments = {}
+        )
+
+    return {
+        'status_Code' : 200,
+        'body' : json.dumps("Glue job successfuly triggered")
+    }
     
-local_data = json.loads(requests.get("https://random-data-api.com/api/bank/random_bank?size=5").text)
-
-s3 = boto3.client('s3')
-resp=s3.get_object(Bucket=bucket, Key=s3_path)
-data=resp.get('Body')
-
-json_data = json.loads(data)
-json_data.append(local_data)
-s3.put_object(Bucket=bucket, Key=s3_path, Body=json.dumps(json_data).encode())
